e04fd28774b6cc34bd16c545d8f5c8dddef399f9
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-07-14 10:24:04 +0300

    я метрика - цели

--- components/YandexMetrika.js
+++ components/YandexMetrika.js
@@ -1,11 +1,24 @@
 "use client";
 import { YMInitializer } from "react-yandex-metrika";
 import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 const YANDEX_ID = [103022816];
 
 const YandexMetrika = () => {
   const pathname = usePathname();
+
+  const trackGoal = (goalName) => {
+    if (typeof window !== "undefined" && window.ym) {
+      window.ym(YANDEX_ID[0], "reachGoal", goalName);
+    }
+  };
+
+  useEffect(() => {
+    if (pathname === "/thanks") {
+      trackGoal("goalLeads");
+    }
+  }, [pathname]);
 
   return (
     <YMInitializer
