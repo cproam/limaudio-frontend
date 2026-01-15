990609e6be721c5866a646a8c30884601c671271
Denis Sergeev <denis-sergeev-85@inbox.ru>
2026-01-15 10:57:52 +0300

    исправление отправки форм

--- app/api/sendForm/route.ts
+++ app/api/sendForm/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
-
+//отправка заявок
 export async function POST(req: NextRequest) {
   const {
     aboutForm,
